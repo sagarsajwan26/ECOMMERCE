f5018e6115f26d853a458074992a6b25be823f4c
sagarsajwan26 <sagarsajwan26@gmail.com>
2025-07-25 16:03:38 +0530

    persisor

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Provider } from 'react-redux'
-import { store } from './store/store.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { Provider } from 'react-redux';
+import { store, persistor } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
-createRoot(document.getElementById('root')).render(
-  <>
-    <Provider store={store}>
-
-    <App />
-    </Provider>
-  </>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,16 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage
 
-import productSlice from './product/productSlice'
-import userSlice from './user/userSlice'
-import adminSlice from './admin/adminSlice'
-import sellerSlice from './seller/sellerSlice'
-import reviewSlice from './review/reviewSlice'
-export const store= configureStore({
-   reducer:{
-     user:userSlice,
-    admin:adminSlice,
-    seller:sellerSlice,
-    products:productSlice,
-    review:reviewSlice
-   }
-})
+import productSlice from './product/productSlice';
+import userSlice from './user/userSlice';
+import adminSlice from './admin/adminSlice';
+import sellerSlice from './seller/sellerSlice';
+import reviewSlice from './review/reviewSlice';
+
+const sellerPersistConfig = {
+  key: 'seller',
+  storage,
+  whitelist: ['logginSeller', 'ordersDetails'] // persist only these fields
+};
+
+export const store = configureStore({
+  reducer: {
+    user: userSlice,
+    admin: adminSlice,
+    seller: persistReducer(sellerPersistConfig, sellerSlice),
+    products: productSlice,
+    review: reviewSlice
+  }
+});
+
+export const persistor = persistStore(store);
